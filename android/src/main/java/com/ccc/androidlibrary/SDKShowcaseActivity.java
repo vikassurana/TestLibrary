73f7b2ec0d3fd5f9b6f2fcab77f15db1cf299058
vikassurana <68941358+vikassurana@users.noreply.github.com>
2020-10-19 09:15:29 -0400

    Completed till overlays
    Completed till overlays

--- android/src/main/java/com/ccc/androidlibrary/SDKShowcaseActivity.java
+++ android/src/main/java/com/ccc/androidlibrary/SDKShowcaseActivity.java
@@ -3,6 +3,7 @@ package com.ccc.androidlibrary;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -14,9 +15,13 @@ import com.cccis.sdk.android.common.helper.MessageHelper;
 import com.cccis.sdk.android.common.provider.RunTimeVariableProvider;
 import com.cccis.sdk.android.domain.legacy.ExpressEstimateWorkflowState;
 import com.cccis.sdk.android.photocapturelocalstorage.QELocalStorageCapturedPhotoService;
+import com.cccis.sdk.android.services.callback.BaseCCCAPIRequestCallback;
 import com.cccis.sdk.android.services.legacy.CapturedPhotoService;
 import com.cccis.sdk.android.services.rest.context.ENVFactory;
+import com.cccis.sdk.android.services.rest.request.VehicleServiceRequest;
 import com.cccis.sdk.android.upload.MCEPClientService;
+import com.cccis.sdk.android.vindecode.CCCAPIVinDecodeClientService;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 public class SDKShowcaseActivity extends LogSupportActivity {
     private static final int DEMO_ACTIVITY_REQUEST_CODE = 100;
@@ -43,6 +48,9 @@ public class SDKShowcaseActivity extends LogSupportActivity {
         buttons = (LinearLayout)findViewById(R.id.buttons);
 
         RunTimeVariableProvider.setImageCollectionKey(DemoConstants.IMAGE_COLLECTION_KEY);
+//        String vehicleType = getIntent().getStringExtra("vehicleType");
+//        Log.i("vehicleType ", "vehicleType " + vehicleType);
+
 
         try {
             mcepClientService = new MCEPClientService(ENVFactory.getInstance(this).SHARED_ENV);
@@ -118,6 +126,49 @@ public class SDKShowcaseActivity extends LogSupportActivity {
         if(authenticated()) {
             Intent intent = new Intent(this, SDKShowcaseVinScanActivity.class);
             startActivity(intent);
+        }
+    }
+
+    public void vinDecode(View view) throws Exception {
+        final String vin = "WP0AA2994VS320240";  //JHLRD68545C011932
+        if(authenticated()) {
+            CCCAPIVinDecodeClientService service = new CCCAPIVinDecodeClientService(ENVFactory.getInstance(this).SHARED_ENV);
+            VehicleServiceRequest request = new VehicleServiceRequest();
+            request.setVin(vin);
+            service.vindecodeDataCenter(request, new BaseCCCAPIRequestCallback(){
+                @Override
+                public TypeReference getSuccessTypeReference() {return new TypeReference<Object>() {};}
+
+                @Override
+                public void onSuccess(Object o) {
+                    //ActivityHelper.showMessage(SDKShowcaseActivity.this, o + "");
+                    Log.i("onSuccess of library", "Decode success! " + o);
+                    int index = o.toString().indexOf("bodyTypeCode")+13;
+                    Log.i("onSuccess of library", "Index is: " + index);
+                    String subString = o.toString().substring(index);
+                    Log.i("onSuccess of library", "SubString is: " + subString);
+                    int secondIndex = subString.indexOf(",");
+                    Log.i("onSuccess of library", "secondIndex is: " + secondIndex);
+                    String vehicleType = subString.substring(0,secondIndex);
+                    Log.i("onSuccess of library", "vehicleType is: " + vehicleType);
+                }
+
+                @Override
+                public void onFailure(Object o, int statusCode, Throwable throwable) {
+                    Log.i("onFailure of library", "Object: " + o);
+                    Log.i("onFailure of library", "statusCode: " + statusCode);
+                    Log.i("onFailure of library", "throwable: " + throwable);
+                }
+
+//                @Override
+//                public void onSuccess(VinDecodeResponseDataCenter result) {
+//                    //handle success
+//                }
+//                @Override
+//                public void onFailure(RESTErrorResponse result, int statusCode, Throwable t) {
+//                    //handle failure
+//                }
+            });
         }
     }
 
